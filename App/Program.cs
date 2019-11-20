8a4d9f1f4bbd9aef515e5bf33b305cf4d5e6d9b5
Andrew Arnott <andrewarnott@gmail.com>
2019-11-20 12:37:01 -0700

    First stab at getting this to work on .NET Core

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+#if NETCOREAPP
+using System.Runtime.Loader;
+#endif
 
 namespace App
 {
@@ -6,9 +9,20 @@ namespace App
     {
         static void Main(string[] args)
         {
+#if NETCOREAPP
+            AssemblyLoadContext.Default.Resolving += AssemblyLoadContext_Resolving;
+#endif
+
             const string message = "hi";
             Console.WriteLine($"V2 serialized: {Convert.ToBase64String(MsgPackV2Consumer.Serialize(message))}");
             Console.WriteLine($"V1 serialized: {Convert.ToBase64String(MsgPackV1Consumer.Serialize(message))}");
         }
+
+#if NETCOREAPP
+        private static System.Reflection.Assembly AssemblyLoadContext_Resolving(AssemblyLoadContext arg1, System.Reflection.AssemblyName arg2)
+        {
+            return null;
+        }
+#endif
     }
 }
